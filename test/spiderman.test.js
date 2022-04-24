687638db5cc81c0d30073a861b5ec6664420527d
Migue Eduardo Castillo Landeros <36210519+meclajer@users.noreply.github.com>
2022-04-24 17:32:36 -0500

    new function added to UT of Spiderman and not pas

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -2,7 +2,7 @@ const Spiderman = require("../app/Spiderman");
 
 describe("UT for spiderman class", () => {
 
-    test('Case 1 Dummy', () => {
+    test('Case 1 create Spiderman object', () => {
 
         // objt to test
         const adrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "sony")
@@ -14,6 +14,11 @@ describe("UT for spiderman class", () => {
         expect(adrewGarfield.movies).toBe(2)
         expect(adrewGarfield.studio).toBe("sony")
 
+    });
+
+    test('Case 2 method getInfo()', () => {
+        const tomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
+        expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
     });
 
 })
